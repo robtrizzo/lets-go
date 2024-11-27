48b656914dc08e1cec487098596c383b4b5431ad
Robert Rizzo <robtrizzo@gmail.com>
2024-11-26 23:26:45 -0500

    partials

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,6 +12,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 	files := []string{
 		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 	ts, err := template.ParseFiles(files...)
@@ -20,7 +21,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
