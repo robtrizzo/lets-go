0a2968161be6a64539ee7b214ec12146ea06fde7
Robert Rizzo <robtrizzo@gmail.com>
2024-11-26 23:56:59 -0500

    static assets and cmd config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+// Last left off on page 77 of let's go
+
 func main() {
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "staticDir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	log.Printf("starting server on %s", cfg.addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 
 }
